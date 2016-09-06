0230b4c5a5d31a07fa3a0392cab9a560fdb940b0
dsharko <sharko.daniel@gmail.com>
2016-09-06 18:02:27 +0300

    added search controller

--- src/main/java/movieappspring/controller/SearchController.java
+++ src/main/java/movieappspring/controller/SearchController.java
@@ -10,39 +10,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.sql.SQLException;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/home")
-public class HomeController {
+@RequestMapping(value = "/search")
+public class SearchController {
 
     private static final String DEFAULT_PAGE_AS_STRING = "1";
     private static final Integer RECORDS_PER_PAGE = 5;
     private MovieService movieService;
 
     @Autowired
-    public HomeController(MovieService movieService) {
+    public SearchController(MovieService movieService) {
         this.movieService = movieService;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView movie(@RequestParam(value = "page", defaultValue = DEFAULT_PAGE_AS_STRING) Integer page) throws SQLException {
+    public ModelAndView searchMovie(@RequestParam(value = "searchInput", defaultValue = "") String searchInput,
+                                    @RequestParam(value = "page", defaultValue = DEFAULT_PAGE_AS_STRING) Integer page) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home");
+        modelAndView.setViewName("searchresult");
 
         if (page <= 0) {
             // TODO some logic
         }
 
-        PagedEntity pagedMovies = movieService.getAllMoviesLimit((page - 1) * RECORDS_PER_PAGE, RECORDS_PER_PAGE);
+        PagedEntity pagedMovies = movieService.searchMovies(searchInput, (page - 1) * RECORDS_PER_PAGE, RECORDS_PER_PAGE);
         List<Movie> movies = (List<Movie>) pagedMovies.getEntity();
         int numberOfRecords = pagedMovies.getNumberOfRecords();
         int numberOfPages = (int) Math.ceil(numberOfRecords * 1.0 / RECORDS_PER_PAGE);
         modelAndView.addObject("movies", movies);
         modelAndView.addObject("numberOfPages", numberOfPages);
         modelAndView.addObject("currentPage", page);
+        modelAndView.addObject("searchRequest", searchInput);
         return modelAndView;
     }
+
 
 }
